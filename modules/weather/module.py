0485a30526569186ff7d4a563820408438dbf782
Prin'Meshia <vallee.jonathan@outlook.com>
2019-12-30 20:15:00 +0100

    update weather module

--- modules/weather/module.py
+++ modules/weather/module.py
@@ -1,19 +1,22 @@
-from PySide2 import QtWidgets,QtCore
+from PySide2 import QtWidgets, QtCore
 import json
 import requests
 import sys
 
+
 class Weather(QtWidgets.QWidget):
-    def __init__(self,parent,config):
-        super(Weather,self).__init__(parent)
+    def __init__(self, parent, config):
+        super(Weather, self).__init__(parent)
         self.baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
         self.setOWMUrl(config)
         if self.fullUrl != False:
-            self.loadWeather()
-        # print( self.fullUrl) 
+            weather = self.getWeather()
+            print(weather['main']['temp'])
+            print(weather)
+        # print( self.fullUrl)
         # response = request.GET()
-    
-    def setOWMUrl(self,config):
+
+    def setOWMUrl(self, config):
         if (not config["city_name"] or config["city_name"] == "") or (not config["city_code"] or config["city_code"] == ""):
             print('no appid or no city selected')
             self.fullUrl = False
@@ -22,13 +25,10 @@ class Weather(QtWidgets.QWidget):
             if config["city_name"] or config["city_name"] != "":
                 self.fullUrl += "&q="+config["city_name"]
             if config["city_code"] or config["city_code"] != "":
-                self.fullUrl += "&id="+config["city_code"]   
-                
-    def loadWeather(self):
-        try:
-            response = requests.get(self.fullUrl)
-        except
-            print()
-       
+                self.fullUrl += "&id="+config["city_code"]
+
+    def getWeather(self):
+        r = requests.get(self.fullUrl)
+        return r.json()
         
         
